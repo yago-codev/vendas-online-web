0e5381a6d09d9d5e7fccaca31784552e4149f806
yago-codev <yagomilano92@gmail.com>
2024-05-20 12:45:12 -0300

    feat: criando página de Login

---
+++ src/modules/login/components/screens/LoginScreen.tsx
@@ -0,0 +1,13 @@
+import * as S from '../styles/LoginScreen.styles'
+
+import backgroundImage from '@public/background.png'
+
+const LoginScreen = () => {
+  return (
+    <S.BackgroundImage
+      src={backgroundImage}
+    />
+  )
+}
+
+export default LoginScreen
---
+++ src/modules/login/index.ts
@@ -0,0 +1,3 @@
+import LoginScreen from "./components/screens/LoginScreen";
+
+export default LoginScreen
